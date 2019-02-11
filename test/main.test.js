41287e0e6f860f32d1061dc4ee149cd7499c100d
stonefarmer9 <g.p.levaillant@gmail.com>
2019-02-11 10:36:37 +0000

    2nd test - Withdrawal decreases balance - written & failing

--- test/main.test.js
+++ test/main.test.js
@@ -1,11 +1,17 @@
 const Account = require('../scripts/main.js')
 
 
-describe('Account', ()=> {
+describe('Account', () => {
 
-  test('#Deposit increases the account balance', ()=>{
-    const myAccount = new Account
+  const myAccount = new Account
+
+  test('#Deposit increases the account balance', () => {
     myAccount.deposit(100)
     expect(myAccount.balance).toBe(100)
+  })
+
+  test('#Withdraw decreases the account balance', () => {
+    myAccount.withdraw(50)
+    expect(myAccount.balance).toBe(50)
   })
 })

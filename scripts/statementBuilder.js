67618f89fc0e02ba4c5635dafb62111cfd8096c6
stonefarmer9 <g.p.levaillant@gmail.com>
2019-03-22 21:45:05 +0000

    Test: createDebit adds a debit statement line to the statement array

--- scripts/statementBuilder.js
+++ scripts/statementBuilder.js
@@ -1,15 +1,21 @@
 class StatementBuilder {
   constructor(log) {
     this.log = log;
-    this.header = "||date||credit||debit||balance||\n";
+    this.statement =["||date||credit||debit||balance||\n"];
+
   }
 
   createCredit() {}
 
-  createDebit() {}
+  createDebit(transaction) {
+  const debit = `||${transaction.date}||` +
+    `     ||£${transaction.amount}||£${transaction.balanceAfter}||\n`;
+    this.statement.push(debit)
+
+  }
 
   build() {
-    return `${this.header}||12/02/2019||£1000||     ||£1000||\n||12/02/2019||     ||£1000||£1000||`
+    return `${this.statement}||12/02/2019||£1000||     ||£1000||\n||12/02/2019||     ||£1000||£1000||`
   }
 
 }
--- test/statementBuilder.test.js
+++ test/statementBuilder.test.js
@@ -4,6 +4,7 @@ const StatementBuilder = require('../scripts/statementBuilder');
 describe('statementBuilder', ()=> {
 
   let log;
+  let debit;
 
   beforeEach(() => {
     let log = [{
@@ -17,11 +18,24 @@ describe('statementBuilder', ()=> {
   			"date": "12/02/2019",
   			"transactionType": "debit"}
     ];
-
+    debit = {
+      "amount": 1000,
+      "balanceAfter": 1000,
+      "date": "12/02/2019",
+      "transactionType": "debit"}
   })
 
   it('returns a completed bank statement', () => {
     builder = new StatementBuilder(log)
     expect(builder.build()).toMatch("||date||credit||debit||balance||\n||12/02/2019||£1000||     ||£1000||\n||12/02/2019||     ||£1000||£1000||")
+  })
+
+  describe("#createDebit", () => {
+    it("adds a credit transaction to the statement array", () => {
+      builder = new StatementBuilder(debit)
+      builder.createDebit(debit)
+      expect(builder.statement.length).toBe(2)
+      expect(builder.statement[1]).toContain(["||12/02/2019||     ||£1000||£1000||"])
+    })
   })
 })
